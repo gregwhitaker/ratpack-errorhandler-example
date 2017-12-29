869b370c57598caadaff5e6f692f4394cb848c38
gregwhitaker <gwhitake@gmail.com>
2017-12-29 01:26:37 -0800

    Doing work

--- src/main/java/com/github/gregwhitaker/ratpackerrorhandler/example/core/error/BaseFieldException.java
+++ src/main/java/com/github/gregwhitaker/ratpackerrorhandler/example/core/error/BaseFieldException.java
@@ -1,11 +1,14 @@
 package com.github.gregwhitaker.ratpackerrorhandler.example.core.error;
 
+import java.util.List;
+
 public abstract class BaseFieldException extends Throwable {
     private int status = 500;
     private String errorCode;
     private String errorMessage = "An error occurred. Please contact support.";
     private String errorDetail;
     private String errorDetailUrl;
+    private List<FieldExceptionDetail> fieldErrors;
 
     public int getStatus() {
         return status;
@@ -45,5 +48,66 @@ public abstract class BaseFieldException extends Throwable {
 
     public void setErrorDetailUrl(String errorDetailUrl) {
         this.errorDetailUrl = errorDetailUrl;
+    }
+
+    public List<FieldExceptionDetail> getFieldErrors() {
+        return fieldErrors;
+    }
+
+    public void setFieldErrors(List<FieldExceptionDetail> fieldErrors) {
+        this.fieldErrors = fieldErrors;
+    }
+
+    public static class FieldExceptionDetail {
+        private String field;
+        private String errorCode;
+        private String errorMessage;
+        private String errorDetail;
+        private String errorDetailUrl;
+
+        public FieldExceptionDetail(String field, String errorMessage) {
+            this.field = field;
+            this.errorMessage = errorMessage;
+        }
+
+        public String getField() {
+            return field;
+        }
+
+        public void setField(String field) {
+            this.field = field;
+        }
+
+        public String getErrorCode() {
+            return errorCode;
+        }
+
+        public void setErrorCode(String errorCode) {
+            this.errorCode = errorCode;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+
+        public void setErrorMessage(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
+
+        public String getErrorDetail() {
+            return errorDetail;
+        }
+
+        public void setErrorDetail(String errorDetail) {
+            this.errorDetail = errorDetail;
+        }
+
+        public String getErrorDetailUrl() {
+            return errorDetailUrl;
+        }
+
+        public void setErrorDetailUrl(String errorDetailUrl) {
+            this.errorDetailUrl = errorDetailUrl;
+        }
     }
 }
