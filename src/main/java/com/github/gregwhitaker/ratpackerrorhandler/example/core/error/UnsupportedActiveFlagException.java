b78b762b79b7a51d7d67194ef3a1cda47ae54248
gregwhitaker <gwhitake@gmail.com>
2017-12-29 01:48:59 -0800

    Doing work

---
+++ src/main/java/com/github/gregwhitaker/ratpackerrorhandler/example/core/error/UnsupportedActiveFlagException.java
@@ -0,0 +1,9 @@
+package com.github.gregwhitaker.ratpackerrorhandler.example.core.error;
+
+public class UnsupportedActiveFlagException extends BaseFieldException {
+
+    public UnsupportedActiveFlagException() {
+        super(404, "4321", "Validation Exception");
+        addField("active", "4321-1","Value must be either 'true' or 'false'.");
+    }
+}
