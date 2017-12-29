fb64ff082cbcf91cf3ed1c013413714b9d3e96b1
gregwhitaker <gwhitake@gmail.com>
2017-12-29 01:22:12 -0800

    Doing work

--- src/main/java/com/github/gregwhitaker/ratpackerrorhandler/example/core/error/BaseException.java
+++ src/main/java/com/github/gregwhitaker/ratpackerrorhandler/example/core/error/BaseException.java
@@ -1,5 +1,49 @@
 package com.github.gregwhitaker.ratpackerrorhandler.example.core.error;
 
 public abstract class BaseException extends Throwable {
+    private int status = 500;
+    private String errorCode;
+    private String errorMessage = "An error occurred. Please contact support.";
+    private String errorDetail;
+    private String errorDetailUrl;
 
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorDetail() {
+        return errorDetail;
+    }
+
+    public void setErrorDetail(String errorDetail) {
+        this.errorDetail = errorDetail;
+    }
+
+    public String getErrorDetailUrl() {
+        return errorDetailUrl;
+    }
+
+    public void setErrorDetailUrl(String errorDetailUrl) {
+        this.errorDetailUrl = errorDetailUrl;
+    }
 }
