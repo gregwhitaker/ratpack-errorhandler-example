076ba6e921a35d2775cb62be0740ea9c7ee67426
gregwhitaker <gwhitake@gmail.com>
2017-12-29 01:17:14 -0800

    Doing work

--- src/main/java/com/github/gregwhitaker/ratpackerrorhandler/example/core/errorhandler/ErrorModule.java
+++ src/main/java/com/github/gregwhitaker/ratpackerrorhandler/example/core/errorhandler/ErrorModule.java
@@ -4,10 +4,14 @@ import com.google.inject.AbstractModule;
 import ratpack.error.ClientErrorHandler;
 import ratpack.error.ServerErrorHandler;
 
+/**
+ * Guice module responsible for adding error handling capabilities.
+ */
 public class ErrorModule extends AbstractModule {
 
     @Override
     protected void configure() {
+        // Wire the ClientErrorHandler and ServerErrorHandler interfaces to your GlobalErrorHandler instance
         bind(ClientErrorHandler.class).to(GlobalErrorHandler.class);
         bind(ServerErrorHandler.class).to(GlobalErrorHandler.class);
     }
